17b503721c84f9cbdd1b23ddbb4cee374f85a911
jashDevkar <devkarjash1054@gmail.com>
2025-04-16 12:21:28 +0530

    merged conflict with main branch

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// export const url = 'https://startsy.onrender.com/'
+export const url = 'https://startsy.onrender.com/'
 // export const url = 'http://192.168.1.7:5002/'
-export const url = 'http://10.10.27.68:5002/'
+// export const url = 'http://10.10.27.68:5002/'
 
 // export const url = "http://172.18.4.79:5002/"
 
